0cf35baa36511e816d058b40367486c010885143
Tanakorn666 <s6506021620059@email.kmutnb.ac.th>
2024-02-20 11:20:18 +0700

    M

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -8,7 +8,7 @@ const path = require("path");
 var bodyParser = require('body-parser');
 
 
-
+//const base_url = "http://localhost:3000";
 const base_url = "http://node56818-tanakorn-noderest.proen.app.ruk-com.cloud";
 
 app.set("views", path.join(__dirname, "/public/views"))
